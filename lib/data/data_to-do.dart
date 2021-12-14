aff4658cb43ac9a8bc2873289524e1810a6e94bb
GabrielBassoi <gabrielbassoi62@gmail.com>
2021-12-13 21:04:43 -0300

    Mod. main and data to-do

--- lib/data/data_to-do.dart
+++ lib/data/data_to-do.dart
@@ -3,24 +3,30 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:sevnotes2/models/todo.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DataTodo {
-  Future<File> getFile() async {
-    final dic = await getApplicationDocumentsDirectory();
-    return File("${dic.path}/data_to-do.json");
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<SharedPreferences> getFile() async {
+    SharedPreferences prefs = await _prefs;
+    return prefs;
   }
 
   Future saveData(List<Todo> list) async {
-    List listJSON = list.map((todo) => todo.toJson()).toList();
-    var data = json.encode(listJSON);
-    final file = await getFile();
-    file.writeAsString(data);
+    try {
+      List data = list.map((todo) => todo.toJson()).toList();
+      final file = await getFile();
+      await file.setStringList("todo-list", data);
+    } catch (e) {
+      print(e);
+    }
   }
 
   Future<List<Todo>> readData() async {
     try {
       final file = await getFile();
-      var data = json.decode(await file.readAsString());
+      var data = await file.getStringList("todo-list");
       List<Todo> list = data.map<Todo>((todo) => Todo.fromJson(todo)).toList();
       return list;
     } catch (e) {
@@ -29,3 +35,29 @@ class DataTodo {
     }
   }
 }
+
+// Antigo
+
+// Future<File> getFile() async {
+//     final dic = await getApplicationDocumentsDirectory();
+//     return File("${dic.path}/data_to-do.json");
+//   }
+
+//   Future saveData(List<Todo> list) async {
+//     List listJSON = list.map((todo) => todo.toJson()).toList();
+//     var data = json.encode(listJSON);
+//     final file = await getFile();
+//     file.writeAsString(data);
+//   }
+
+//   Future<List<Todo>> readData() async {
+//     try {
+//       final file = await getFile();
+//       var data = json.decode(await file.readAsString());
+//       List<Todo> list = data.map<Todo>((todo) => Todo.fromJson(todo)).toList();
+//       return list;
+//     } catch (e) {
+//       print(e);
+//       return [];
+//     }
+//   }
